3bae718bc376bd6880c5f47d22ff216be0ee0138
emirPuskaITAkademija <emir.puska@it-akademija.com>
2026-02-10 18:30:00 +0100

    Init commit

---
+++ src/main/java/com/academy/Main.java
@@ -0,0 +1,9 @@
+package com.academy;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
